83f7c9439adfe7f32fa171523b467b103ef24b5b
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-20 21:50:11 -0700

    Done.

--- index.js
+++ index.js
@@ -7,22 +7,62 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   const url = `https://api.github.com/repos/${repo}/forks`
-  fetch(url)
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
   .then(response => response.json())
   .then(repositories => {
     showResults(repositories)
   })
 }
 
+
 function showResults(json) {
-  debugger
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}> Go to - ${json.name}</a>`
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const username = 'NInman112'
+  const repo = `${username}/js-ajax-fetch-lab`
+  const url = `https://api.github.com/repos/${repo}/issues`
+  const postData = {
+    title: document.getElementById('title').value,
+    body: issueBody = document.getElementById('body').value
+  }
+  // const issueTitle = document.getElementById('title').value
+  // const issueBody = document.getElementById('body').value
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(response => response.json())
+  .then(json => getIssues());
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const username = 'NInman112'
+  const repo = `${username}/js-ajax-fetch-lab`;
+  const url = `https://api.github.com/repos/${repo}/issues`;
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(response => response.json())
+    .then(json => showIssues(json));
+}
+
+function showIssues(json) {
+  showIssues = document.getElementById('issues')
+  for (var i = 0, len = json.length; i < len; i++) {
+    showIssues.innerHTML +=
+    `<p>${json[i].body}</p>` +
+    `<p>${json[i].user.log}</p>`
+  }
 }
